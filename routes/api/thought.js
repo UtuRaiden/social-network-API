2b7871b4d611cebfe28e0ea902f9aee382022734
unknown <barronpeterson56@gmail.com>
2023-08-15 13:34:39 -0500

    add all routes

---
+++ routes/api/thought.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    createThought,
+    getThoughts,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:id
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
